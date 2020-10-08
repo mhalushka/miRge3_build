6aa4ba70c946ca34725629f9ab2ba4ddb1d4f74b
arunhpatil <arun26feb@gmail.com>
2020-10-08 14:31:37 -0400

    modified the link to GitHub

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,11 +58,16 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-
+html_show_sourcelink = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_context = {
+        'display_github': True,
+        'github_user': 'arunhpatil',
+        'github_repo': 'miRge3_build',
+        'github_version': 'master/docs/source/',
+    }
